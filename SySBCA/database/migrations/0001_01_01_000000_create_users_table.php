32f6ea57ca922eae7e2536e08e885c4f152bff9e
follyjames <follyjames70@gmail.com>
2025-06-25 15:24:48 +0000

    Création des migrations

--- SySBCA/database/migrations/0001_01_01_000000_create_users_table.php
+++ SySBCA/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,19 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('utilisateurs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->enum('etat', ['actif', 'suspendu'])->default('actif');
+            $table->foreignId('role_id')
+                ->constrained('roles')
+                ->default(3);
+            $table->unsignedBigInteger('entity_id')->nullable(); // qui veut dire que si le role est fs, 4 correspondra à l'enregistrement 4 de la table fs
+            $table->boolean('doit_renitialiser_pwd')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
